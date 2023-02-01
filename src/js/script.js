19d5da21502a058ebacc73774f63a9e4da040afb
The5thAxiom <samridh.anand.paatni@gmail.com>
2023-02-01 11:47:32 +0530

    added utility functions

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,31 @@ console.log("hacker's horizon");
 
 const id = id => document.getElementById(id);
 // Adding a time delay after clicking the register now button
-document.getElementById("register-button").addEventListener("click", function(){
-    setTimeout(function(){
-        window.open("https://www.vitchennaievents.com/conf1/index.php?eventid=1217", "_blank");
-    }, 0100);
-    // 0100 milliseconds = 0.1 seconds
-});
+document
+    .getElementById('register-button')
+    .addEventListener('click', function () {
+        setTimeout(function () {
+            window.open(
+                'https://www.vitchennaievents.com/conf1/index.php?eventid=1217',
+                '_blank'
+            );
+        }, 100);
+        // 0100 milliseconds = 0.1 seconds
+    });
+
+const tags = tag => {
+    let elements = [];
+    for (let element of document.getElementsByTagName(className)) {
+        elements.push(element);
+    }
+    return elements;
+};
+
+const classes = className => {
+    let elements = [];
+    for (let element of document.getElementsByClassName(className)) {
+        elements.push(element);
+    }
+    return elements;
+};
+
