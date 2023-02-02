1a1ae6339bf5b31d902ec2184ab90d7aeead07bb
The5thAxiom <samridh.anand.paatni@gmail.com>
2023-02-02 21:42:54 +0530

    refactored register buttons

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,6 @@
 console.log("hacker's horizon");
 
 const id = id => document.getElementById(id);
-// Adding a time delay after clicking the register now button
-document
-    .getElementById('register-button')
-    .addEventListener('click', function () {
-        setTimeout(function () {
-            window.open(
-                'https://www.vitchennaievents.com/conf1/index.php?eventid=1217',
-                '_blank'
-            );
-        }, 100);
-        // 0100 milliseconds = 0.1 seconds
-    });
 
 const tags = tag => {
     let elements = [];
